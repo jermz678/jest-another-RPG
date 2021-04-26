f2f1aba902a19c65fe92469138301c020e94f13d
jermz678 <jermz678@live.com>
2021-04-26 14:07:26 -0500

    inventory and stats'

--- _tests_/Player.test.js
+++ _tests_/Player.test.js
@@ -6,6 +6,7 @@ console.log(new Potion());
 
 
 const Player = require('../lib/Player');
+const { test, expect } = require('@jest/globals');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -18,3 +19,24 @@ test('creates a player object', () => {
         expect.arrayContaining([expect.any(Object)])
       );
 })
+
+test('gets players stats as an object', () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+
+});
