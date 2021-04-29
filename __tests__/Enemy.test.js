de3abfa5463266f182a09c0c5647a9a0d9860bbc
jermz678 <jermz678@live.com>
2021-04-28 22:03:27 -0500

    starting game logic

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -50,3 +50,10 @@ test("gets enemy's health value", () => {
   
     expect(enemy.health).toBe(0);
   });
+
+  test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+  });
